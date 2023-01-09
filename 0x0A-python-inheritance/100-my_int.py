7316991e4a93949507e3c355092ddffab5e39bfd
Emmanuel Aji Agbo <emmanuelaji32@gmail.com>
2023-01-09 14:27:01 +0100

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""this module defines a class MyInt that inherits from int"""
+"""
+Contains the class MyInt
+"""
 
 
 class MyInt(int):
-    """Invert int operators == and !="""
+    """rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
-    def __eq__(self, value):
-        """Override == opeartor with != behavior"""
-        return self.real != value
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
 
-    def __ne__(self, value):
-        """Override != operator with == behavior"""
-        return self.real == value
-
+    def __ne__(self, other):
+        """what was == is now !="""
+        return int(self) == other
