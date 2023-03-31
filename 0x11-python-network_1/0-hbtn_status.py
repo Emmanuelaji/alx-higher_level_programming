2908beb6d89ff175a421dc48ca0896dab9433f73
Emmanuel Aji Agbo <breeze@localhost.localdomain>
2023-03-31 20:59:57 +0100

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@ import urllib.request
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as respones:
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("|\t- type: {}".format(type(body)))
+        print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ found in the header of the response.
 if __name__ == "__main__":
     import sys
     import urllib.request
-    
+
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.headers["X-Request-Id"])
