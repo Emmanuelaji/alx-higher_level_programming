39078ac50867442e91ca490cb3988ca4ba3f8fcd
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2023-03-31 04:02:34 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of the response
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# send a GET request to the URL using curl, and display the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
